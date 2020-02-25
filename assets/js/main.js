a0b8e6afc11a68608ca15497708bcfd36010b316
LucasHe4rt <lucassilvabittencourt@protonmail.ch>
2020-02-25 11:19:09 -0300

    fix slider mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
 
-    $("#slider").slick()
+    $("#slider").slick({
+        responsive: [
+            {
+                breakpoint: 540,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true,
+                  arrows: false,
+                }
+            }
+        ],
+        centerPadding: '100px',
+    });
 
 })
