8faff7175562b1c8a6b8b7d9e3729d414d36f86e
LucasHe4rt <lucassilvabittencourt@protonmail.ch>
2020-02-25 10:12:58 -0300

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,5 @@
 $(document).ready(function () {
 
-    $("#send").click(function () {
-
-        $.ajaxSetup({
-
-            headers: {
-
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-
-            }
-
-        });
-
-        $.post('/send',{
-            name: $("#name").val(),
-            email: $("#email").val(),
-            subject: $("#subject").val(),
-            message: $("#message").val()
-        },
-            function (data, status) {
-
-            console.log(data, status);
-
-        });
-
-    })
+    $("#slider").slick()
 
 })
